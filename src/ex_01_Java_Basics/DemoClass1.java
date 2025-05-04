25e0a9287f66019d178789d5e55ab03a9e14c4d4
TejuVinny <teju.asan@gmail.com>
2025-05-04 12:06:22 +0530

    Commiting Demo class to Project

---
+++ src/ex_01_Java_Basics/DemoClass1.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basics;
+
+public class DemoClass1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World! Welcome to Testing Academy !!!!- from the DemoClass1");
+    }
+}
