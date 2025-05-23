8e8243d9d7421b70fba6f1642b4dbe866e9949c8
TejuVinny <teju.asan@gmail.com>
2025-05-23 16:32:15 +0530

    23 may 2025

---
+++ src/ex_01_Java_Basics/Variable.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basics;
+
+public class Variable {
+    public static void main(String[] args) {
+        byte age = 31;
+        int i = 10;
+        i = 20+16;
+        System.out.println("Age of me:" +age);
+        System.out.println("Age of me:" +i);
+    }
+}
