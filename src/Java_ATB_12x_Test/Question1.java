a1ddec48d6d902798477089b675b81dcd93ff357
TejuVinny <teju.asan@gmail.com>
2025-06-21 11:58:25 +0530

    23 may 2025: Java Question1

---
+++ src/Java_ATB_12x_Test/Question1.java
@@ -0,0 +1,15 @@
+package Java_ATB_12x_Test;
+
+public class Question1 {
+    public static void main(String[] args) {
+        int _in = 25;
+        double _dbl = 3.14;
+        boolean _bool = true;
+        char _ch = 'A';
+        System.out.println("Integer:" + _in);
+        System.out.println("Double:" + _dbl);
+        System.out.println("Boolean:" + _bool);
+        System.out.println("Character:" + _ch);
+
+    }
+}
