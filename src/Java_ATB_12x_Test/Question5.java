f4abde5fe3fb16ba6465a34d7161af0577ae6b2b
TejuVinny <teju.asan@gmail.com>
2025-06-21 12:50:33 +0530

    23 may 2025: Java Question5

---
+++ src/Java_ATB_12x_Test/Question5.java
@@ -0,0 +1,24 @@
+package Java_ATB_12x_Test;
+
+public class Question5 {
+    public static void main(String[] args) {
+
+
+        System.out.println("operator precedence");
+
+        //Typecasting
+
+        //Implicit
+        int i= 10;
+        double d = i;
+
+        //Explicit
+        double dbl = 3.5;
+        int myint = (int) dbl;
+
+        System.out.println("/nType casting");
+        System.out.println("Implicit type casting" +d);
+        System.out.println("Explicit type casting" + myint);
+
+    }
+}
