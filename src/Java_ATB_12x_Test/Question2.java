f8820cb072e1285c5da8df5a84d2a2c2cbca44de
TejuVinny <teju.asan@gmail.com>
2025-06-21 12:07:06 +0530

    23 may 2025: Java Question2

---
+++ src/Java_ATB_12x_Test/Question2.java
@@ -0,0 +1,26 @@
+package Java_ATB_12x_Test;
+
+public class Question2 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b =5;
+        //Arithmetic Operators
+        System.out.println( "Arithmetic Operators:");
+
+        //Addition
+        System.out.println( "Addition Operators: a + b = " +(a+b));
+
+        //Subtraction
+        System.out.println( "Subtraction Operators: a - b = " +(a-b));
+
+        //Multiplication
+        System.out.println( "Multiplication Operators: a * b = " +(a*b));
+
+        //Division
+        System.out.println( "Division Operators:a / b = " +(a/b));
+
+        //Modulus
+        System.out.println( "Modulus Operators:a % b = " +(a%b));
+    }
+}
