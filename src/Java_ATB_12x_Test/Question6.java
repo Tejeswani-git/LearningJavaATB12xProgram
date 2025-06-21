170722a5a2f2f2be52de007a4e22518eac9946ff
TejuVinny <teju.asan@gmail.com>
2025-06-21 12:56:34 +0530

    23 may 2025: Java Question6

---
+++ src/Java_ATB_12x_Test/Question6.java
@@ -0,0 +1,21 @@
+package Java_ATB_12x_Test;
+
+import java.util.Scanner;
+
+public class Question6 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = 15;
+        if (n > 0) {
+            System.out.println("The num is Positive Number");
+        } else if (n < 0){
+            System.out.println("The num is Negative Num");
+    }else
+
+    {
+        System.out.println("The num is Zero Num");
+    }
+    scanner.close();
+}
+}
