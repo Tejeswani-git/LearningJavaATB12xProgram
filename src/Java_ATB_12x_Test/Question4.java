311d0f768a5c9679a6c027b1cdfce7526afe262b
TejuVinny <teju.asan@gmail.com>
2025-06-21 12:28:51 +0530

    23 may 2025: Java Question4

---
+++ src/Java_ATB_12x_Test/Question4.java
@@ -0,0 +1,19 @@
+package Java_ATB_12x_Test;
+
+public class Question4 {
+    public static void main(String[] args) {
+        int a =5;
+
+        System.out.println("Original :" +a);
+        System.out.println("Pre-increment :" +(++a));
+
+        System.out.println("Post-increment :" +(a++));
+
+//int b=5;
+        System.out.println("Pre-decrement :" +(--a));
+
+
+        System.out.println("Post-decrement :" +(a--));
+
+            }
+}
