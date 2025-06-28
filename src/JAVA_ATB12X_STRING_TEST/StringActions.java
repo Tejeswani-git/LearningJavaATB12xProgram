21013eb8265af2eba9f647ed6d883677a2861f17
TejuVinny <teju.asan@gmail.com>
2025-06-28 11:00:40 +0530

    28 June 2025: Java String-Test_Question1

---
+++ src/JAVA_ATB12X_STRING_TEST/StringActions.java
@@ -0,0 +1,12 @@
+package JAVA_ATB12X_STRING_TEST;
+
+public class StringActions {
+    public static void main(String[] args) {
+
+        String s = "HELLO WORLD";
+        System.out.println("First Character : "+ s.charAt(0));
+        System.out.println(s.length());
+        s.substring(6);
+        System.out.println(s);
+    }
+}
